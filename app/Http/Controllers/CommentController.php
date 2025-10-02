512905dead751c39ebff1e67e57186e745e9f4ed
moosbeere <moosbeere_O@mail.ru>
2025-10-02 16:05:04 +0300

    Lab 8 begin

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use App\Mail\Commentmail;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
+
 
 
 class CommentController extends Controller
@@ -18,7 +21,8 @@ class CommentController extends Controller
         $comment-> text = $request->text;
         $comment->article_id = $request->article_id;
         $comment->user_id = auth()->id();
-        $comment->save();
+        if($comment->save())
+            Mail::to('moosbeere_O@mail.ru')->send(new Commentmail($comment, $request->article_id));
         return redirect()->route('article.show', $request->article_id)->with('message', "Comment add succesful");
     }
 
